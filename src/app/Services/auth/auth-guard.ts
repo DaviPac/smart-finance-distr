3c11635ad7521b50603fd7f1b3c7a2e74e8611ff
Davi <131999650+DaviPac@users.noreply.github.com>
2025-10-21 19:22:15 -0300

    fix redirect login

--- src/app/Services/auth/auth-guard.ts
+++ src/app/Services/auth/auth-guard.ts
@@ -2,22 +2,19 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from './auth';
 import { UsersService } from '../user/user';
+import { firstValueFrom } from 'rxjs';
 
 export const authGuard: CanActivateFn = async (_route, _state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
-  const usersService = inject(UsersService)
+  const usersService = inject(UsersService);
 
-  if (authService.isAuthenticated()) {
-    const authenticatedUser = authService.currentFirebaseUser();
-    if (!authenticatedUser) {
-      router.navigate(['/login']);
-      return false;
-    }
+  const authenticatedUser = await firstValueFrom(authService.user$);
+
+  if (authenticatedUser) {
     const userData = await usersService.getUserById(authenticatedUser.uid)
     if (!userData) {
-      router.navigate(['/login']);
-      return false;
+      return router.parseUrl('/login');
     }
     authService.currentUser.set(userData);
     return true;
