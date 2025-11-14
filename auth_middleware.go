71bc94ec8bda585758c5aefadfdb763f8673caa5
Davi <131999650+DaviPac@users.noreply.github.com>
2025-11-14 17:13:43 -0300

    fix: authMiddleware now reads token from authorization header

--- auth_middleware.go
+++ auth_middleware.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"net/http"
+	"strings"
 
 	"github.com/golang-jwt/jwt/v5"
 )
@@ -19,16 +20,16 @@ type SessionClaims struct {
 func (app *AppConfig) authMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		cookie, err := r.Cookie("session_token")
-		if err != nil {
+		authHeader := r.Header.Get("Authorization")
+		if !strings.HasPrefix(authHeader, "Bearer ") {
 			http.Error(w, "Não autorizado: Token não encontrado", http.StatusUnauthorized)
 			return
 		}
 
-		tokenString := cookie.Value
+		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
 		claims := &SessionClaims{}
 
-		token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (any, error) {
 			return app.JWTSecret, nil
 		})
 
